319c0a1def38a70ce2262091d220ff47df6c7bfb
aallison <aallison@yahoo-inc.com>
2014-06-07 02:12:47 -0700

    initial commit

---
+++ Open Frameworks/OF Objective C++ Class.xctemplate/___FILEBASENAME___.h
@@ -0,0 +1,20 @@
+#pragma once
+
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___FULLUSERNAME___. All rights reserved.
+//
+
+#include "ofMain.h"
+
+class ___FILEBASENAMEASIDENTIFIER___ {
+
+public:
+    ___FILEBASENAMEASIDENTIFIER___();
+  
+private:
+
+};
---
+++ Open Frameworks/OF Objective C++ Class.xctemplate/___FILEBASENAME___.mm
@@ -0,0 +1,13 @@
+//
+//  ___FILENAME___
+//  ___PROJECTNAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//  Copyright (c) ___YEAR___ ___FULLUSERNAME___. All rights reserved.
+//
+
+#include "___FILEBASENAME___.h"
+
+___FILEBASENAMEASIDENTIFIER___::___FILEBASENAMEASIDENTIFIER___() {
+
+}
